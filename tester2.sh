5ec19c5df59018da9de91ab75506a3f07de5c245
Administrator <admin@example.com>
2021-06-11 03:40:09 +0000

    Update tester2.sh

--- tester2.sh
+++ tester2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
   
-echo '{"email":"wilton@tester.com.br","comment":"this is dead","content_id":1}' > teste2.json
+echo '{"email":"wilton@tester.com.br","comment":"this is alive","content_id":2}' > teste2.json
 curl -sv 192.168.1.114:38000/api/comment/new -X POST -H 'Content-Type: application/json' -d @teste2.json
-curl -sv --silent 192.168.1.114:38000/api/comment/list/2 |grep 'this is dead' > output2.txt
+curl -sv --silent 192.168.1.114:38000/api/comment/list/2 |grep 'this is alive' > output2.txt
 
 arquivo="output2.txt"
 if [ -s $arquivo ]
@@ -10,5 +10,5 @@ then
    echo "teste yes"
 else
    echo "teste no"
-   exit 1 
+   exit 1
 fi
